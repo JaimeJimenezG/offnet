3093036f0839258595bfa72315522566d0f1571c
Jaime Jimenez <jaimejimenezg@hotmail.es>
2024-10-07 20:35:27 +0200

    added default path for database

--- src-tauri/src/db/connection.rs
+++ src-tauri/src/db/connection.rs
@@ -8,9 +8,8 @@ pub type DbPool = Arc<r2d2::Pool<ConnectionManager<SqliteConnection>>>;
 
 pub fn establish_connection() -> DbPool {
     dotenv().ok();
-
     let database_url = env::var("DATABASE_URL")
-        .expect("La variable de entorno DATABASE_URL debe estar configurada");
+        .unwrap_or_else(|_| "sqlite:offnet.db".to_string());
     
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
